da51d2b8df0122fe62fb3a4b6e3abc795fe35961
batin.eryilmaz <batin.eryilmaz@trendyol.com>
2021-03-24 13:33:48 +0300

    update images

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -5,8 +5,8 @@ import SEO from "../components/seo"
 import Slider from "react-slick"
 import "../styles/index.scss"
 import { GrFormNext, GrFormPrevious } from "react-icons/gr"
-import MenuItem from "../components/MenuItem"
 
+import Img from "gatsby-image"
 const Menu = () => {
   const [settings, setSettings] = useState({
     dots: false,
@@ -31,104 +31,33 @@ const Menu = () => {
   }, [])
 
   const data = useStaticQuery(graphql`
-    query {
-      menu1: file(relativePath: { eq: "Menu1.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      menu2: file(relativePath: { eq: "Menu2.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      menu3: file(relativePath: { eq: "Menu3.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      menu4: file(relativePath: { eq: "Menu4.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      menu5: file(relativePath: { eq: "Menu5.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      menu6: file(relativePath: { eq: "Menu6.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      menu7: file(relativePath: { eq: "Menu7.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      menu8: file(relativePath: { eq: "Menu8.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      menu9: file(relativePath: { eq: "Menu9.png" }) {
-        childImageSharp {
+  query {
+    allImageSharp(filter: {fluid: {originalName: {regex: "/Menu-/"}}}, sort: {order: ASC, fields: fixed___originalName}) {
+      edges {
+        node {
           fluid {
             ...GatsbyImageSharpFluid
           }
         }
       }
     }
+  }
   `)
+
+  const images = () => {
+    return data.allImageSharp.edges.map(item => {
+      return (
+        <div>
+          <Img fluid={item.node.fluid} />
+        </div>)
+    })
+  }
 
   return (
     <Layout>
       <SEO title="Menü" />
       <Slider className="slider" {...settings}>
-        <div>
-          <MenuItem imageUrl={data.menu1.childImageSharp.fluid.src} />
-        </div>
-        <div>
-          <MenuItem imageUrl={data.menu2.childImageSharp.fluid.src} />
-        </div>
-        <div>
-          <MenuItem imageUrl={data.menu3.childImageSharp.fluid.src} />
-        </div>
-        <div>
-          <MenuItem imageUrl={data.menu4.childImageSharp.fluid.src} />
-        </div>
-        <div>
-          <MenuItem imageUrl={data.menu5.childImageSharp.fluid.src} />
-        </div>
-        <div>
-          <MenuItem imageUrl={data.menu6.childImageSharp.fluid.src} />
-        </div>
-        <div>
-          <MenuItem imageUrl={data.menu7.childImageSharp.fluid.src} />
-        </div>
-        <div>
-          <MenuItem imageUrl={data.menu8.childImageSharp.fluid.src} />
-        </div>
-        <div>
-          <MenuItem imageUrl={data.menu9.childImageSharp.fluid.src} />
-        </div>
+        {images()}
       </Slider>
     </Layout>
   )
